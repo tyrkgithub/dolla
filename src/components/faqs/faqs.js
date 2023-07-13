066f299fcfc545d838d52d34848ccd9648d55307
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-07-13 12:39:57 +0100

    updated faqs

--- src/components/faqs/faqs.js
+++ src/components/faqs/faqs.js
@@ -12,11 +12,11 @@ function Faqs() {
       <ul className="faqs-ul">
         <li className="faqs-li">
           <p className="faqs-q">IS IT SAFE?</p>
-          <p className="faqs-a">Billy is a fully qualifed and insured technecian with over 2 years experience of tooth gem application. The gems cause no harm on the off chance that one is accidently eaten. </p>
+          <p className="faqs-a">Billy is a fully qualifed and insured technecian with over 2 years experience of tooth gem application. The gems cause no harm to the teeth or gums and are completely safe if accidently eaten. </p>
         </li>
         <li className="faqs-li">
           <p className="faqs-q">HOW LONG WILL THE GEM STAY?</p>
-          <p className="faqs-a">The gems can last over a year and most will usually last for .......... All gems come with a grace period of .......... meaning  </p>
+          <p className="faqs-a">The gems can last over a year and most will usually last for 6 months. In the unlikely event that the gem comes off witin the first 4 weeks you are covered by Billys 1 month grace period. This means that she would fit a replacement for free *This excludes crystal dust and gold*. </p>
         </li>
         <li className="faqs-li">
           <p className="faqs-q">WHAT TIMES AND DATES ARE AVAILABLE?</p>
@@ -27,8 +27,8 @@ function Faqs() {
           <p className="faqs-a">Bookings are taken through Instagram and Facebook messages. Please note - a deposit is required at the time of booking. </p>
         </li>
         <li className="faqs-li">
-          <p className="faqs-q">IS THE LOCATION ACCESSABLE?</p>
-          <p className="faqs-a">...............................................</p>
+          <p className="faqs-q">IS THE LOCATION ACCESSIBLE?</p>
+          <p className="faqs-a">The studio is not on the ground floor but Hamilton House has 2 lifts. Billy is happy to discuss accessibility with you please don't hesitate to get in touch. </p>
         </li>
       </ul>
     
